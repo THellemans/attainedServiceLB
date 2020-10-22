45e97ce0343714bc2eb1aadfafb2a69a013cb2f8
THellemans <tim.hellemans@motulus.be>
2020-10-22 14:00:39 +0200

    Small corrections

--- generate_plots/fig3.m
+++ generate_plots/fig3.m
@@ -2,7 +2,7 @@
 clear variables
 load('../data/fig3ab.mat')
 
-x=lambdas(1:end); y=EW_rel([1 4 5 7], 1:end); legend_entries={"SQ($5$)-RTB",  "LAS($5$)",  "LAS($5$)-QTB",  "LEW($5$)"};
+x=lambdas(1:end); y=EW_rel([1 4 5 7], 1:end); legend_entries={"SQ($20$)-RTB",  "LAS($20$)",  "LAS($20$)-QTB",  "LEW($20$)"};
 legend_loc="southwest"; xLab='$\lambda$'; yLab='$E_{W, rel, P}$'; xLim=[0.3,1]; yLim=[0, 1];
 filename='../figures/fig3a.pdf'; logplot=false;
 
@@ -12,7 +12,7 @@ make_and_save_fig(x, y, legend_entries, legend_loc, xLab, yLab, xLim, yLim, file
 clear variables
 load('../data/fig3ab.mat')
 
-x=lambdas(1:end); y=EW_rel([2 3 6 7], 1:end); legend_entries={"SQ($5$)-RE($2$)",  "SQ($5$)-RTB-RE($2$)",  "RE($5,2$)",  "LEW($5$)"};
+x=lambdas(1:end); y=EW_rel([2 3 6 7], 1:end); legend_entries={"SQ($20$)-RE($2$)",  "SQ($20$)-RTB-RE($2$)",  "RE($20,2$)",  "LEW($20$)"};
 legend_loc="southwest"; xLab='$\lambda$'; yLab='$E_{W, rel, P}$'; xLim=[0.3,1]; yLim=[0, 1];
 filename='../figures/fig3b.pdf'; logplot=false;
 
